08fa0f2f0de2e87f08b57a3b2af85bf08b06d7a2
Stephen Robbins <stephendeanrobbins@gmail.com>
2026-02-28 22:23:23 -0700

    addedDictandErrorHandlingSDR

---
+++ pythonErrorHandling/pythonDictandErrsSDR.py
@@ -0,0 +1,50 @@
+# Define custom exception
+class StudentInfoError(Exception):
+    def __init__(self, message):
+        self.message = message  # Initialize the exception message
+
+def find_ID(name, info):
+    # Type your code here.
+    foundID = info.get(name)
+    if foundID:
+        return foundID
+    else:
+        raise StudentInfoError(f'Student ID not found for {name}')
+    
+def find_name(ID, info):
+    # Type your code here.
+
+    for item, value in info.items():
+        if ID == value:
+            found_name = item
+            return found_name
+        else:
+            raise StudentInfoError(f'Student name not found for {ID}')
+    
+
+
+if __name__ == '__main__':
+    # Dictionary of student names and IDs
+    student_info = {
+        'Reagan' : 'rebradshaw835',
+        'Ryley' : 'rbarber894',
+        'Peyton' : 'pstott885',
+        'Tyrese' : 'tmayo945',
+        'Caius' : 'ccharlton329'
+    }
+    
+    userChoice = input()    # Read search option from user. 0: find_ID(), 1: find_name()
+    
+    # FIXME: find_ID() and find_name() may raise an Exception.
+    #        Insert a try/except statement to catch the exception and output any exception message.
+    try:
+        if userChoice == "0":
+            name = input()
+            result = find_ID(name, student_info)
+        else:
+            ID = input()
+            result = find_name(ID, student_info)
+        print(result)
+    except StudentInfoError as s:
+        print(s)
+    
